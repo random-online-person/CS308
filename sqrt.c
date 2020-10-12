5254c9863ea8deac40ad25da917caaf2056b3d7f
random-online-person <yash.tg00@gmail.com>
2020-10-12 20:01:35 +0530

    Step 1.33 Added a Is_valid_num function

--- sqrt.c
+++ sqrt.c
@@ -4,7 +4,21 @@ positive number. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
+bool Is_valid_num(char str[]) {
+	for (int i = 0; i < strlen(str) ; i++)
+	if (!isdigit(str[i])){
+		return false;
+	}
+	int temp = atoi(str);
+	if(temp < 0){
+		return false;
+	}
+    return true;
+}
 // this is step 1.7
 
 // this is entry point of the  program
@@ -16,14 +30,21 @@ int main(int argc, char* argv[]) {
 		exit(-1);
 	}
 
-	//else print the sqrt of the input number
-	int input = atoi(argv[1]);
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	if(Is_valid_num(argv[1])){
+		//else print the sqrt of the input number
+		int input = atoi(argv[1]);
+		printf("Sqrt of %d is %f\n",input,sqrt(input));
+	} else{
+		printf("Enter a valid number \n\n");
+	}
+
 
 
 	//step 1.18
-	printf("End of program. Exiting. \n");
+	printf("End of program. Exiting. \n\n");
 
 	return(0);
 
 } // end main
+
+
